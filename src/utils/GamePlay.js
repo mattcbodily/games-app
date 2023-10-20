c5f84197b91c2c21b87ab4ff0cbe7f23189b3b3a
mattcbodily <mattcbodily@gmail.com>
2023-10-19 21:15:17 -0600

    Adds more comments

--- src/utils/GamePlay.js
+++ src/utils/GamePlay.js
@@ -2,4 +2,5 @@ import { TTT_WIN_CONDITIONS } from '../constants/Game'
 
 export const checkForWinner = (playerOneCells, playerTwoCells) => {
   // figure this out so that even when a player has other cells selected, if they have a win scenario this should still return true
+  // Check win condition cells for the same value?
 }
