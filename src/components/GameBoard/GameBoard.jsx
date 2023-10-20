b6b69df7ce6aa8dc04fbdfa5f2f830bdae5120b4
mattcbodily <mattcbodily@gmail.com>
2023-10-19 20:58:03 -0600

    moves game cell styles

--- src/components/GameBoard/GameBoard.jsx
+++ src/components/GameBoard/GameBoard.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 
 import { TTT_NEW_GAME_BOARD } from '../../constants/Game'
 
+const gameCellStyles = 'h-28 w-28 border-2 rounded-md border-primary flex justify-center items-center text-6xl active:bg-primary-focus'
+
 export const GameBoard = ({ playerTurn, setPlayerTurn }) => {
   const [gameBoard, setGameBoard] = useState(TTT_NEW_GAME_BOARD)
 
@@ -38,15 +40,15 @@ export const GameBoard = ({ playerTurn, setPlayerTurn }) => {
 
   return (
     <div className='h-fit grid grid-cols-3 grid-rows-3 gap-1'>
-      <div className='h-28 w-28 border-2 rounded-md border-black flex justify-center items-center text-6xl active:bg-primary-focus' onClick={() => onCellClick(0)}>{gameBoard[0]}</div>
-      <div className='h-28 w-28 border-2 rounded-md border-black flex justify-center items-center text-6xl active:bg-primary-focus' onClick={() => onCellClick(1)}>{gameBoard[1]}</div>
-      <div className='h-28 w-28 border-2 rounded-md border-black flex justify-center items-center text-6xl active:bg-primary-focus' onClick={() => onCellClick(2)}>{gameBoard[2]}</div>
-      <div className='h-28 w-28 border-2 rounded-md border-black flex justify-center items-center text-6xl active:bg-primary-focus' onClick={() => onCellClick(3)}>{gameBoard[3]}</div>
-      <div className='h-28 w-28 border-2 rounded-md border-black flex justify-center items-center text-6xl active:bg-primary-focus' onClick={() => onCellClick(4)}>{gameBoard[4]}</div>
-      <div className='h-28 w-28 border-2 rounded-md border-black flex justify-center items-center text-6xl active:bg-primary-focus' onClick={() => onCellClick(5)}>{gameBoard[5]}</div>
-      <div className='h-28 w-28 border-2 rounded-md border-black flex justify-center items-center text-6xl active:bg-primary-focus' onClick={() => onCellClick(6)}>{gameBoard[6]}</div>
-      <div className='h-28 w-28 border-2 rounded-md border-black flex justify-center items-center text-6xl active:bg-primary-focus' onClick={() => onCellClick(7)}>{gameBoard[7]}</div>
-      <div className='h-28 w-28 border-2 rounded-md border-black flex justify-center items-center text-6xl active:bg-primary-focus' onClick={() => onCellClick(8)}>{gameBoard[8]}</div>
+      <div className={gameCellStyles} onClick={() => onCellClick(0)}>{gameBoard[0]}</div>
+      <div className={gameCellStyles} onClick={() => onCellClick(1)}>{gameBoard[1]}</div>
+      <div className={gameCellStyles} onClick={() => onCellClick(2)}>{gameBoard[2]}</div>
+      <div className={gameCellStyles} onClick={() => onCellClick(3)}>{gameBoard[3]}</div>
+      <div className={gameCellStyles} onClick={() => onCellClick(4)}>{gameBoard[4]}</div>
+      <div className={gameCellStyles} onClick={() => onCellClick(5)}>{gameBoard[5]}</div>
+      <div className={gameCellStyles} onClick={() => onCellClick(6)}>{gameBoard[6]}</div>
+      <div className={gameCellStyles} onClick={() => onCellClick(7)}>{gameBoard[7]}</div>
+      <div className={gameCellStyles} onClick={() => onCellClick(8)}>{gameBoard[8]}</div>
     </div>
   )
 }
