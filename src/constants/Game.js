3791d0182f0bc1d2a94e1ce259b1cceda8dc83df
mattcbodily <mattcbodily@gmail.com>
2023-10-19 19:46:14 -0600

    Adds comments

--- src/constants/Game.js
+++ src/constants/Game.js
@@ -1,3 +1,6 @@
+// Maybe add themes for game pieces? 9 emoji's in each theme
+export const GAME_PIECES = ['🦙', '🐢', '🦖', '🦊', '🐴', '🦄', '🐊', '❌', '⭕']
+
 export const NEW_GAME_BOARD = ['', '', '', '', '', '', '', '', '']
 
 export const WIN_SCENARIOS = [
