4b3182c177e496700ba1a3a37f990a28eaac7e9c
mattcbodily <mattcbodily@gmail.com>
2023-10-19 20:53:51 -0600

    Fixes constant reference issue

--- src/components/GameBoard/GameBoard.jsx
+++ src/components/GameBoard/GameBoard.jsx
@@ -1,9 +1,13 @@
 import { useState } from 'react'
 
-import { NEW_GAME_BOARD } from '../../constants/Game'
+import { TTT_NEW_GAME_BOARD } from '../../constants/Game'
 
 export const GameBoard = ({ playerTurn, setPlayerTurn }) => {
-  const [gameBoard, setGameBoard] = useState(NEW_GAME_BOARD)
+  const [gameBoard, setGameBoard] = useState(TTT_NEW_GAME_BOARD)
+
+  // Find a different way to do this that can share the gameboard state?
+  const [playerOneCells, setPlayerOneCells] = useState([])
+  const [playerTwoCells, setPlayerTwoCells] = useState([])
 
   const changePlayerTurn = () => {
     playerTurn === '🦙' ? setPlayerTurn('🐢') : setPlayerTurn('🦙')
@@ -17,20 +21,32 @@ export const GameBoard = ({ playerTurn, setPlayerTurn }) => {
     gameBoardSlice.splice(cellNumber, 1, playerTurn)
 
     setGameBoard(gameBoardSlice)
+
+    if (playerTurn === '🦙') {
+      setPlayerOneCells([...playerOneCells, cellNumber])
+    } else {
+      setPlayerTwoCells([...playerTwoCells, cellNumber])
+    }
+
+    // Check for winner
+
     changePlayerTurn()
   }
 
+  console.log('1', playerOneCells)
+  console.log('2', playerTwoCells)
+
   return (
     <div className='h-fit grid grid-cols-3 grid-rows-3 gap-1'>
-      <div className='h-28 w-28 border-2 rounded-md border-black flex justify-center items-center text-6xl active:bg-primary' onClick={() => onCellClick(0)}>{gameBoard[0]}</div>
-      <div className='h-28 w-28 border-2 rounded-md border-black flex justify-center items-center text-6xl active:bg-primary' onClick={() => onCellClick(1)}>{gameBoard[1]}</div>
-      <div className='h-28 w-28 border-2 rounded-md border-black flex justify-center items-center text-6xl active:bg-primary' onClick={() => onCellClick(2)}>{gameBoard[2]}</div>
-      <div className='h-28 w-28 border-2 rounded-md border-black flex justify-center items-center text-6xl active:bg-primary' onClick={() => onCellClick(3)}>{gameBoard[3]}</div>
-      <div className='h-28 w-28 border-2 rounded-md border-black flex justify-center items-center text-6xl active:bg-primary' onClick={() => onCellClick(4)}>{gameBoard[4]}</div>
-      <div className='h-28 w-28 border-2 rounded-md border-black flex justify-center items-center text-6xl active:bg-primary' onClick={() => onCellClick(5)}>{gameBoard[5]}</div>
-      <div className='h-28 w-28 border-2 rounded-md border-black flex justify-center items-center text-6xl active:bg-primary' onClick={() => onCellClick(6)}>{gameBoard[6]}</div>
-      <div className='h-28 w-28 border-2 rounded-md border-black flex justify-center items-center text-6xl active:bg-primary' onClick={() => onCellClick(7)}>{gameBoard[7]}</div>
-      <div className='h-28 w-28 border-2 rounded-md border-black flex justify-center items-center text-6xl active:bg-primary' onClick={() => onCellClick(8)}>{gameBoard[8]}</div>
+      <div className='h-28 w-28 border-2 rounded-md border-black flex justify-center items-center text-6xl active:bg-primary-focus' onClick={() => onCellClick(0)}>{gameBoard[0]}</div>
+      <div className='h-28 w-28 border-2 rounded-md border-black flex justify-center items-center text-6xl active:bg-primary-focus' onClick={() => onCellClick(1)}>{gameBoard[1]}</div>
+      <div className='h-28 w-28 border-2 rounded-md border-black flex justify-center items-center text-6xl active:bg-primary-focus' onClick={() => onCellClick(2)}>{gameBoard[2]}</div>
+      <div className='h-28 w-28 border-2 rounded-md border-black flex justify-center items-center text-6xl active:bg-primary-focus' onClick={() => onCellClick(3)}>{gameBoard[3]}</div>
+      <div className='h-28 w-28 border-2 rounded-md border-black flex justify-center items-center text-6xl active:bg-primary-focus' onClick={() => onCellClick(4)}>{gameBoard[4]}</div>
+      <div className='h-28 w-28 border-2 rounded-md border-black flex justify-center items-center text-6xl active:bg-primary-focus' onClick={() => onCellClick(5)}>{gameBoard[5]}</div>
+      <div className='h-28 w-28 border-2 rounded-md border-black flex justify-center items-center text-6xl active:bg-primary-focus' onClick={() => onCellClick(6)}>{gameBoard[6]}</div>
+      <div className='h-28 w-28 border-2 rounded-md border-black flex justify-center items-center text-6xl active:bg-primary-focus' onClick={() => onCellClick(7)}>{gameBoard[7]}</div>
+      <div className='h-28 w-28 border-2 rounded-md border-black flex justify-center items-center text-6xl active:bg-primary-focus' onClick={() => onCellClick(8)}>{gameBoard[8]}</div>
     </div>
   )
 }
--- src/constants/Game.js
+++ src/constants/Game.js
@@ -1,8 +1,20 @@
+/* 
+  Constant prefix game legend:
+  - TTT -> Tic Tac Toe
+*/
+
 // Maybe add themes for game pieces? 9 emoji's in each theme
 export const GAME_PIECES = ['🦙', '🐢', '🦖', '🦊', '🐴', '🦄', '🐊', '❌', '⭕']
 
-export const NEW_GAME_BOARD = ['', '', '', '', '', '', '', '', '']
+export const TTT_NEW_GAME_BOARD = ['', '', '', '', '', '', '', '', '']
 
-export const WIN_SCENARIOS = [
-  // add the win scenarios
+export const TTT_WIN_CONDITIONS = [
+  // Row win conditions
+  [0, 1, 2], [3, 4, 5], [6, 7, 8],
+
+  // Column win conditions
+  [0, 3, 6], [1, 4, 7], [2, 5, 8],
+
+  // Diagonal win conditions
+  [0, 4, 8], [2, 4, 6]
 ]
