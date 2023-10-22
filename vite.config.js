2673d1e4df2fabbadef51fe62e48f3db7da1c5ba
mattcbodily <mattcbodily@gmail.com>
2023-10-22 09:58:57 -0600

    Adds purpose field to icons in manifest

--- vite.config.js
+++ vite.config.js
@@ -28,7 +28,8 @@ export default defineConfig({
           {
             "src": "/icon-192x192.png",
             "sizes": "192x192",
-            "type": "image/png"
+            "type": "image/png",
+            "purpose": "any"
           },
           {
             "src": "/maskable_icon_x192.png",
@@ -39,17 +40,20 @@ export default defineConfig({
           {
             "src": "/icon-256x256.png",
             "sizes": "256x256",
-            "type": "image/png"
+            "type": "image/png",
+            "purpose": "any"
           },
           {
             "src": "/icon-384x384.png",
             "sizes": "384x384",
-            "type": "image/png"
+            "type": "image/png",
+            "purpose": "any"
           },
           {
             "src": "/icon-512x512.png",
             "sizes": "512x512",
-            "type": "image/png"
+            "type": "image/png",
+            "purpose": "any"
           }
         ],
       },
