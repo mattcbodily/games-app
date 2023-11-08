0867359acb461f21a3f7561c86b50afbbd3d3a0c
mattcbodily <mattcbodily@gmail.com>
2023-11-07 19:20:39 -0700

    Adjusts layout

--- src/components/PlayerSelect/PlayerSelect.jsx
+++ src/components/PlayerSelect/PlayerSelect.jsx
@@ -18,21 +18,21 @@ export const PlayerSelect = () => {
 
   return (
     <div className='h-screen w-full flex flex-col items-center'>
-      <div className='w-[290px] mt-8 mb-8 flex justify-between'>
-        <div className='h-8 flex flex-col items-center'>
-          <p className='text-4xl'>{playerOne}</p>
-          <span className='text-lg'>Player 1</span>
-        </div>
-        <div className='flex flex-col items-center'>
-          <p className='text-4xl'>{playerTwo}</p>
-          <span className='text-lg'>Player 2</span>
-        </div>
-      </div>
       <p className='text-2xl mb-8'>{`Player ${playerOne ? 'two' : 'one'}, choose an emoji!`}</p>
       <div className='h-fit grid grid-cols-3 grid-rows-3 gap-1'>
         {GAME_PIECES.map(piece => (
           <div className={`h-28 w-28 border-2 rounded-md border-primary flex justify-center items-center text-6xl cursor-pointer ${playerOne === piece || playerTwo === piece ? 'bg-primary' : ''} active:bg-primary-focus`} key={piece} onClick={() => handlePlayerSelection(piece)}>{piece}</div>
         ))}
+      </div>
+      <div className='h-16 w-[290px] mt-8 mb-8 flex justify-between'>
+        <div className='h-8 flex flex-col items-center'>
+          <span className='text-lg'>Player 1</span>
+          <p className='text-4xl'>{playerOne}</p>
+        </div>
+        <div className='flex flex-col items-center'>
+          <span className='text-lg'>Player 2</span>
+          <p className='text-4xl'>{playerTwo}</p>
+        </div>
       </div>
       <Link 
         className='btn btn-primary mt-8' 
